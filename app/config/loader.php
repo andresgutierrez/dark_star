9a6e2d636ca9e34c6e1661414bb422e6d212efd6
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2014-05-17 14:12:02 -0500

    fix

--- app/config/loader.php
+++ app/config/loader.php
@@ -1,5 +1,6 @@
 <?php
 
 // Prepend a base path if Predis is not available in your "include_path".
-require '../app/library/Predis/autoload.php';
+require '../library/Predis/autoload.php';
+
 Predis\Autoloader::register();
